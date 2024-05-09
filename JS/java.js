5d148b02c6d7569e3fbbce646e8960c7d56b53de
Matheus-Sousa-Ferreira <msousaferreira0@gmail.com>
2024-05-09 11:58:40 -0300

    Add files via upload

---
+++ JS/java.js
@@ -0,0 +1,15 @@
+function pagmulher(n){
+    var nome = n;
+    sessionStorage.setItem('nome', nome);
+    window.location.href = "mulher.html";
+
+}
+
+
+
+
+function paghomem (n){
+    var nome = n;
+    sessionStorage.setItem('nome', nome);
+    window.location.href = "homem.html";
+}
